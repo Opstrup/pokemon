514b403caf38538c5a5ce0fae271b6e00edd96c3
Anders Opstrup <aho@magnetix.dk>
2022-06-01 21:12:21 +0200

    Initial Commit

---
+++ pokemon/pokemonApp.swift
@@ -0,0 +1,17 @@
+//
+//  pokemonApp.swift
+//  pokemon
+//
+//  Created by Anders Opstrup on 01/06/2022.
+//
+
+import SwiftUI
+
+@main
+struct pokemonApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
